bc5f0795ebe88f349bc5016dd1feb693cd2b1f6a
Mauricio Rogerio Obenaus <mobenaus@gmail.com>
2025-07-25 13:53:28 -0300

    Implementação da concorrencia

--- internal/report/report.go
+++ internal/report/report.go
@@ -6,11 +6,16 @@ import (
 	"github.com/mobenaus/fc-pos-go-desafio-stress-test/internal/stress"
 )
 
-func DisplayReport(str stress.StressTestResults) {
+func DisplayReport(str *stress.StressTestResults) {
 	fmt.Printf("Stress Test executado em %v\n", str.TotalTime)
 	fmt.Printf("Total requests........: %v\n", str.TotalRequests)
 	fmt.Printf("Total Success requests: %v\n", str.Total200Requests)
-	for code, total := range str.TotalStatusMap {
-		fmt.Printf("Total %v requests....: %v\n", code, total)
-	}
+	str.TotalStatusMap.Range(func(code, total any) bool {
+		if totalInt64, ok := total.(*int64); ok {
+			fmt.Printf("Total %v requests....: %v\n", code, *totalInt64)
+		}
+		return true
+	})
+	fmt.Printf("Requests por segundo %f\n", float64(str.TotalRequests)/str.TotalTime.Seconds())
+
 }
--- internal/stress/stress-test.go
+++ internal/stress/stress-test.go
@@ -2,6 +2,9 @@ package stress
 
 import (
 	"context"
+	"fmt"
+	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/mobenaus/fc-pos-go-desafio-stress-test/internal/request"
@@ -15,9 +18,9 @@ type StressTest struct {
 
 type StressTestResults struct {
 	TotalTime        time.Duration
-	TotalRequests    int
-	Total200Requests int
-	TotalStatusMap   map[int]int
+	TotalRequests    int64
+	Total200Requests int64
+	TotalStatusMap   sync.Map
 }
 
 func NewStressTest(
@@ -31,33 +34,69 @@ func NewStressTest(
 	}
 }
 
-func (st *StressTest) Execute(ctx context.Context) (StressTestResults, error) {
+func (st *StressTest) Execute(ctx context.Context) (*StressTestResults, error) {
 
-	result := StressTestResults{
-		TotalStatusMap: make(map[int]int),
+	result := &StressTestResults{
+		TotalStatusMap: sync.Map{},
 	}
 	request, error := request.NewStressRequest(st.url)
 	if error != nil {
-		return StressTestResults{}, error
+		return result, error
 	}
 
 	start := time.Now()
 
-	result.TotalRequests++
+	defer func() {
+		result.TotalTime = time.Since(start)
+	}()
+
+	rc := make(chan int)
+	var wg sync.WaitGroup
+
+	for range st.concurrency {
+		go func() {
+			for {
+				<-rc
+				err := st.callRequest(result, request)
+				if err != nil {
+					fmt.Println(err.Error())
+				}
+				wg.Done()
+			}
+		}()
+	}
+
+	var i int
+
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			fmt.Printf("Called %d requests...\r", i)
+		}
+	}()
+
+	for i = 0; i < st.requests; i++ {
+		wg.Add(1)
+		rc <- i
+	}
+	wg.Wait()
+	return result, nil
+}
+
+func (st *StressTest) callRequest(result *StressTestResults, request *request.StressRequest) error {
+	atomic.AddInt64(&result.TotalRequests, 1)
 
 	status, error := request.Execute()
 	if error != nil {
-		return StressTestResults{}, error
+		return error
 	}
 
 	if status == 200 {
-		result.Total200Requests++
+		atomic.AddInt64(&result.Total200Requests, 1)
+	} else {
+		val, _ := result.TotalStatusMap.LoadOrStore(status, new(int64))
+		ptr := val.(*int64)
+		atomic.AddInt64(ptr, 1)
 	}
-	result.TotalStatusMap[status]++
-
-	finish := time.Now()
-
-	result.TotalTime = finish.Sub(start)
-
-	return result, nil
+	return nil
 }
